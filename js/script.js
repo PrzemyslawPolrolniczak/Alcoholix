f8b49f86f38692c7d213bbdb9e3db2c2ae6926c4
Przemyslaw Polrolniczak <przemekp96@gmail.com>
2017-02-20 16:07:31 +0100

    start of the project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+  var tr1 = $('.triangle1');
+  var tr2 = $('.triangle2');
+  var tr3 = $('.triangle3');
+  var tr4 = $('.triangle4');
+  
+  tr1.hover(function(){
+    $(this).addClass("rectangle");
+  })
+});
