74fee15fccbea9c9b872d62518d5f55e14d0a7f4
Przemyslaw Polrolniczak <przemekp96@gmail.com>
2017-02-21 19:35:36 +0100

    changed whole menu animation, added links, menu is fully functional now

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(function(){
     tr1.addClass("rectangle");
     tr1.removeClass("triangle1");
     tr1.css("left", "-180px");
-    tr1.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr1.animate({'opacity': 0}, 50, function () { tr1.css("width", "360px"),
     tr1.text('1 Gracz');}).animate({'opacity': 1}, 1000);
   });
   
@@ -19,7 +19,7 @@ $(function(){
     tr2.addClass("rectangle altBackground");
     tr2.removeClass("triangle2");
     tr2.css("left", "187px");
-    tr2.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr2.animate({'opacity': 0}, 50, function () { tr2.css("width", "360px"),
     tr2.text('2 Graczy');}).animate({'opacity': 1}, 1000);
   });
   
@@ -28,7 +28,7 @@ $(function(){
     tr3.removeClass("triangle3");
     tr3.css("left", "-180px");
     tr3.css("top", "187px");
-    tr3.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr3.animate({'opacity': 0}, 50, function () { tr3.css('width', "360px"),
     tr3.text('3 Graczy');}).animate({'opacity': 1}, 1000);
   });
   
@@ -37,7 +37,8 @@ $(function(){
     tr4.removeClass("triangle4");
     tr4.css("left", "187px");
     tr4.css("top", "187px");
-    tr4.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr4.text('');
+    tr4.animate({'opacity': 0}, 50, function () { tr4.css('width', "360px"),
     tr4.text('4 Graczy');}).animate({'opacity': 1}, 1000);
   });
   
