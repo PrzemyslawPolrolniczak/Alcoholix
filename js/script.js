e52292e821580a041f01c2bbc5512ae7e995e35c
Przemyslaw Polrolniczak <przemekp96@gmail.com>
2017-02-21 19:20:57 +0100

    made pretty menu

--- js/script.js
+++ js/script.js
@@ -3,36 +3,42 @@ $(function(){
   var tr2 = $('.triangle2');
   var tr3 = $('.triangle3');
   var tr4 = $('.triangle4');
+  var name = $('.name');
   
-  tr1.hover(function(){
+  $('div.hidden').fadeIn(3000).removeClass('hidden');
+  
+  tr1.one("mouseover", function(){
     tr1.addClass("rectangle");
     tr1.removeClass("triangle1");
-    tr1.animate({width: "360px"}, 1000);
     tr1.css("left", "-180px");
+    tr1.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr1.text('1 Gracz');}).animate({'opacity': 1}, 1000);
   });
   
-  tr2.hover(function(){
-    tr2.addClass("rectangle");
+  tr2.one("mouseover",function(){
+    tr2.addClass("rectangle altBackground");
     tr2.removeClass("triangle2");
-    tr2.animate({width: "360px"}, 1000);
-    tr2.css("background-color", "#00B4CC");
-    tr2.css("left", "180px");
-  });
-  //jak był position absolute to normalnie się rozkładał hmm
-  tr3.hover(function(){
-    tr3.addClass("rectangle");
-    tr3.removeClass("triangle3");
-    tr3.animate({width: "360px"}, 1000);
-    tr3.css("bottom", "7px");
-    tr3.css("background-color", "#00B4CC");
-    tr3.css("left", "-180px");
+    tr2.css("left", "187px");
+    tr2.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr2.text('2 Graczy');}).animate({'opacity': 1}, 1000);
   });
   
-  tr4.hover(function(){
+  tr3.one("mouseover",function(){
+    tr3.addClass("rectangle altBackground");
+    tr3.removeClass("triangle3");
+    tr3.css("left", "-180px");
+    tr3.css("top", "187px");
+    tr3.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr3.text('3 Graczy');}).animate({'opacity': 1}, 1000);
+  });
+  
+  tr4.one("mouseover",function(){
     tr4.addClass("rectangle");
     tr4.removeClass("triangle4");
-    tr4.animate({width: "360px"}, 1000);
-    tr4.css("bottom", "7px");
-    tr4.css("left", "180px");
+    tr4.css("left", "187px");
+    tr4.css("top", "187px");
+    tr4.animate({'opacity': 0, width: "360px"}, 500, function () {
+    tr4.text('4 Graczy');}).animate({'opacity': 1}, 1000);
   });
+  
 });
